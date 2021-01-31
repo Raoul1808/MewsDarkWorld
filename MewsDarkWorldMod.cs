9fbb3c99314289ca4562d842e13fb953835d5bca
Raoul1808 <raoulthegeek@gmail.com>
2021-01-31 12:59:53 +0100

    Initial commit

---
+++ MewsDarkWorldMod.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace MewsDarkWorld
+{
+	public class MewsDarkWorldMod : Mod
+	{
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MewsDarkWorld
+ Simple Terraria Mod made just to explore tModLoader. May end up being a full mod.
